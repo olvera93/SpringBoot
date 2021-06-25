b5270c1475f8fe6a9fd1be321f4f384873079f1d
Olvera93 <olvera9302@outlook.com>
2021-06-25 14:01:04 -0500

    Se agrega validaciones y se cree mi propias anotaciones

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,10 +1,46 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
-public class Usuario {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
+import com.bolsadeideas.springboot.form.app.validation.Requerido;
+
+public class Usuario {
+	
+	//@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+	@IdentificadorRegex // Mi propia anotación
+	private String identificador;
+	
+	//@NotEmpty(message = "El nombre no puede ser vacio")
+	private String nombre;
+	
+	//@NotEmpty
+	@Requerido // Mi propia anotación para validar el apellido
+	private String apellido;
+
+	@NotBlank
+	@Size(min = 3, max = 8)
 	private String username;
+	
+	@NotEmpty
 	private String password;
+	
+	//@NotEmpty
+	@Requerido
+	@Email(message = "El correo con formato incorrecto")
 	private String email;
+	
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
 
 	public String getUsername() {
 		return username;
@@ -29,5 +65,39 @@ public class Usuario {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+
+	public Integer getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
+	}
+	
+	
 
 }
