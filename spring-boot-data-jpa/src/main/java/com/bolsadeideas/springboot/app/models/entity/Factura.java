2cf74cc8faa74461a25e28616120c0198cdf8289
Olvera93 <olvera9302@outlook.com>
2021-07-15 11:16:13 -0500

    Se crea la clase ItemFactura y se mapea con la clase Factura

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -27,17 +32,27 @@ public class Factura implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String descripcion;
 	private String observacion;
-	
+
 	@Temporal(TemporalType.DATE)
 	@Column(name = "create_at")
 	private Date createAt;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	private List<ItemFactura> items;
 	
+	
+
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
+
 	@PrePersist
 	public void prePersist() { // Este metodo se encarga de generar la fecha automaticamente
 		createAt = new Date();
@@ -81,6 +96,18 @@ public class Factura implements Serializable {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+	public void addItemFactura(ItemFactura item) {
+		items.add(item);
 	}
 
 	private static final long serialVersionUID = 1L;
