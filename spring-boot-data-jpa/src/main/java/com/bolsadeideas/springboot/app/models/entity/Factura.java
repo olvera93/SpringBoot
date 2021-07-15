f8e04819492896e9fcd817c9fd04f01643c64f62
Olvera93 <olvera9302@outlook.com>
2021-07-15 11:25:38 -0500

    Se crea la clase Producto y la clase ItemFactura se relaciona con Producto. Se hace un metodo en la clase factura para calcular el precio total de los productos

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.app.models.entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -110,6 +111,19 @@ public class Factura implements Serializable {
 		items.add(item);
 	}
 
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		int size = items.size();
+		
+		for (int i = 0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		
+		return total;
+		
+	}
+	
 	private static final long serialVersionUID = 1L;
 
 }
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/ItemFactura.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/ItemFactura.java
@@ -3,10 +3,18 @@ package com.bolsadeideas.springboot.app.models.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+/**
+ * @author Olvera Monroy Gonzalo
+ *
+ */
 
 @Entity
 @Table(name = "faturas_items")
@@ -17,6 +25,10 @@ public class ItemFactura implements Serializable {
 	private Long id;
 
 	private Integer cantidad;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "producto_id")
+	private Producto producto;
 
 	public Long getId() {
 		return id;
@@ -38,8 +50,8 @@ public class ItemFactura implements Serializable {
 		return serialVersionUID;
 	}
 
-	public Long calcularImporte() {
-		return cantidad.longValue();
+	public Double calcularImporte() {
+		return cantidad.doubleValue() * producto.getPrecio();
 		
 	}
 	
