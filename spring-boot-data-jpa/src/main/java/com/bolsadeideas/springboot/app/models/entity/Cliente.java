5af15b387c480c2cabf0aa63624562ddbb717d34
Olvera93 <olvera9302@outlook.com>
2021-07-15 11:07:30 -0500

    Se crea la clase factura y se relaciona con la clase cliente

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -46,6 +51,17 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createdAt;
 	
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	
+	
+	public Cliente() {
+		
+		facturas = new ArrayList<Factura>();
+	}
+
+
 	private String foto;
 
 	public Long getId() {
@@ -100,6 +116,18 @@ public class Cliente implements Serializable {
 		return serialVersionUID;
 	}
 	
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
 
 
 	private static final long serialVersionUID = 1L;
