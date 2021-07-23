1ca9cb953be02acc169806c70990675e23ed96e4
Olvera93 <olvera9302@outlook.com>
2021-07-23 11:40:58 -0500

    [SPRING SECURITY] se agrega mas seguridad para que un usuario anonimo no pueda crear, editar o eliminar a otro usuario

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,6 +26,12 @@ public class MvcConfig implements WebMvcConfigurer{
 		.addResourceLocations(resourcePath);
 	}
 	*/
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+	
+		registry.addViewController("/error_403").setViewName("error_403");
+		
+	}
 
 	
 }
